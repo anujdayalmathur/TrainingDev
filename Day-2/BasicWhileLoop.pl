8f8f4aa00b4c449e5420c88983c7cd44e03b5f08
Anuj <37199607+anujdayalmathur@users.noreply.github.com>
2018-03-09 16:20:12 +0530

    Update BasicWhileLoop.pl

--- Day-2/BasicWhileLoop.pl
+++ Day-2/BasicWhileLoop.pl
@@ -2,14 +2,15 @@
 
 $counter = 10;
 
-$counterLimit = 20;
+$counterLimit = 20;
+
 # while loop execution
 
 while( $counter <= $counterLimit ) {
 
-   printf "Value of counter: $counter\n";
+   printf "Value of counter: $counter\n";
 
-   $counter = $counter + 1;
+   $counter = $counter + 1;
 }
 
-print "While condition satisfied \n";
+print "While condition satisfied \n";
